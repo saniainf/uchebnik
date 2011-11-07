6740a07a2f112c6bb1f1516a26595fccb616079f
saniainf <saniainf@gmail.com>
2011-11-07 13:34:05 +0700

    array

--- Glava7/Examples.cs
+++ Glava7/Examples.cs
@@ -4,6 +4,39 @@ class Example
 {
     static void Main()
     {
+        string numb;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("1. Простые массивы\n2. Ступенчатые массивы\n3. Break Continue\n\n");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("Введи номер части: ");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        numb = Console.ReadLine();
+        Console.ResetColor();
+        Console.Write("\n\n");
+
+        switch (numb)
+        {
+            case "1":
+                Console.Clear();
+                Chap1();
+                break;
+            case "2":
+                Console.Clear();
+                Chap2();
+                break;
+            case "3":
+                Console.Clear();
+                Chap3();
+                break;
+            default:
+                Console.WriteLine("не вводи всякую херню\n\n\n");
+                Main();
+                break;
+        }
+    }
+
+    static void Chap1()
+    {
         /*масив без инициализации при объявлении*/
         int[] massive = new int[10]; //new для распределения памяти
         for (int a = 0; a < 10; a++)
@@ -25,17 +58,30 @@ class Example
         Console.WriteLine();
         Console.WriteLine();
 
-        /* 3х мерный масив*/
+        /* 3х мерный массив без инициализации при объявлении
+         * 2х мерный можно объявить при инициализации
+         * */
         int[, ,] massive4 = new int[4, 5, 3];
+        int x = 1;
 
         for (int a = 0; a < 4; a++)
             for (int b = 0; b < 5; b++)
                 for (int c = 0; c < 3; c++)
-                    massive4[a, b, c] = b+c;
+                    massive4[a, b, c] = x++;
 
         for (int a = 0; a < 4; a++)
             for (int b = 0; b < 5; b++)
                 for (int c = 0; c < 3; c++)
                     Console.WriteLine("масив[{0},{1},{2}]: {3}", a, b, c, massive4[a, b, c]);
+    }
+
+    static void Chap2()
+    {
+
+    }
+
+    static void Chap3()
+    {
+
     }
 }
