3dd25323bc32ec1706fae5c1d24f813c93ff895c
saniainf <saniainf@gmail.com>
2011-11-07 13:24:48 +0700

    array

--- Glava7/Examples.cs
+++ Glava7/Examples.cs
@@ -11,6 +11,9 @@ class Example
         for (int a = 0; a < 10; a++)
             Console.WriteLine("массив[{0}]: {1}", a, massive[a]);
 
+        Console.WriteLine();
+        Console.WriteLine();
+
         /*масив с инициализацией при объявлении*/
         int[] massive2 = { 99, 10, 100, 18, 78, 23, 63, 9, 87, 49 }; //необходимость в new отпадает
         int avg = 0;
@@ -19,6 +22,20 @@ class Example
         avg /= 10;
         Console.WriteLine("среднее:{0}", avg);
 
-        /**/
+        Console.WriteLine();
+        Console.WriteLine();
+
+        /* 3х мерный масив*/
+        int[, ,] massive4 = new int[4, 5, 3];
+
+        for (int a = 0; a < 4; a++)
+            for (int b = 0; b < 5; b++)
+                for (int c = 0; c < 3; c++)
+                    massive4[a, b, c] = b+c;
+
+        for (int a = 0; a < 4; a++)
+            for (int b = 0; b < 5; b++)
+                for (int c = 0; c < 3; c++)
+                    Console.WriteLine("масив[{0},{1},{2}]: {3}", a, b, c, massive4[a, b, c]);
     }
 }
