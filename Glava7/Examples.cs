cc1313b3420f32e2951bf1235f99831a30a748d3
saniainf <saniainf@gmail.com>
2011-11-07 12:41:01 +0700

    new sania

--- Glava7/Examples.cs
+++ Glava7/Examples.cs
@@ -4,6 +4,19 @@ class Example
 {
     static void Main()
     {
+        /*масив без инициализации при объявлении*/
+        int[] massive = new int[10]; //new для распределения памяти
+        for (int a = 0; a < 10; a++)
+            massive[a] = a;
+        for (int a = 0; a < 10; a++)
+            Console.WriteLine("массив[{0}]: {1}", a, massive[a]);
 
+        /*масив с инициализацией при объявлении*/
+        int[] massive2 = { 99, 10, 100, 18, 78, 23, 63, 9, 87, 49 }; //необходимость в new отпадает
+        int avg = 0;
+        for (int a = 0; a < 10; a++)
+            avg += massive2[a];
+        avg /= 10;
+        Console.WriteLine("среднее:{0}", avg);
     }
 }
