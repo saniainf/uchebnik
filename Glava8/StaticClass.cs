5079584733d0ec3d7e3c7779b7e99c56e8db9fbd
saniainf <saniainf@gmail.com>
2011-11-30 15:28:24 +0700

    add

--- Glava8/StaticClass.cs
+++ Glava8/StaticClass.cs
@@ -1,6 +1,29 @@
 ﻿using System;
 
-static class Servis
+static class MainService
 {
     static int count;
+
+    static public void PrintCount()
+    {
+        PrintF(count);
+    }
+
+    static public void PrintF(string value)
+    {
+        Console.WriteLine(value);
+        count++;
+    }
+
+    static public void PrintF(int value)
+    {
+        Console.WriteLine(value);
+        count++;
+    }
+
+    static public void PrintF()
+    {
+        Console.WriteLine();
+        count++;
+    }
 }
