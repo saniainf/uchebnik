f9c84b4c8f72dc88e35e8df608cb1fcde0fbf17f
saniainf <saniainf@gmail.com>
2011-12-02 17:03:48 +0700

    overload operator

--- Glava9/Chapter1.cs
+++ Glava9/Chapter1.cs
@@ -8,13 +8,46 @@ class Chapter1
         Vector3 beta = new Vector3(10, 10, 10);
         Vector3 c;
 
+        Console.Write("coord alpha:\t");
+        alpha.PrintF();
+
+        Console.Write("coord beta:\t");
+        beta.PrintF();
+
+        Console.Write("alpha + beta:\t");
+        c = alpha + beta;
+        c.PrintF();
+
+        Console.Write("alpha+beta+c:\t");
+        c = alpha + beta + c;
+        c.PrintF();
+
+        Console.Write("c - alpha:\t");
+        c = c - alpha;
+        c.PrintF();
+
+        Console.Write("alpha.X + c.X:\t");
+        c.X = alpha.X + c.X;
+        c.PrintF();
+
+        Console.Write("c++\t\t");
+        c++;
+        c.PrintF();
+
+        Console.Write("c--\t\t");
+        c--;
+        c.PrintF();
+
+        Console.Write("alpha + 40\t");
+        alpha = alpha + 40;
+        alpha.PrintF();
 
     }
 }
 
 class Vector3
 {
-    int X, Y, Z;
+    public int X, Y, Z;
 
     public Vector3()
     {
@@ -54,8 +87,51 @@ class Vector3
         return result;
     }
 
+    /* перегрузка оператора ++ */
+    public static Vector3 operator ++(Vector3 obj1)
+    {
+        Vector3 result = new Vector3();
+
+        /*инкременировать obj1*/
+        result.X = obj1.X + 1;
+        result.Y = obj1.Y + 1;
+        result.Z = obj1.Z + 1;
+        /*возврат объекта*/
+        return result;
+    }
+
+    /* перегрузка оператора -- */
+    public static Vector3 operator --(Vector3 obj1)
+    {
+        Vector3 result = new Vector3();
+
+        /*декременировать obj1*/
+        result.X = obj1.X - 1;
+        result.Y = obj1.Y - 1;
+        result.Z = obj1.Z - 1;
+        /*возврат объекта*/
+        return result;
+    }
+
+    /* перегрузка оператора +
+     *  Vector3 + int
+     *  */
+    public static Vector3 operator +(Vector3 obj1, int a)
+    {
+        Vector3 result = new Vector3();
+
+        /*прибавить к полям obj1 целое число*/
+        result.X = obj1.X + a;
+        result.Y = obj1.Y + a;
+        result.Z = obj1.Z + a;
+        /*возврат объекта*/
+        return result;
+    }
+
     public void PrintF()
     {
-        Console.WriteLine("X: {0}, Y: {1}, Z: {2}", X, Y, Z);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("X: {0},\tY: {1},\tZ: {2}", X, Y, Z);
+        Console.ResetColor();
     }
 }
