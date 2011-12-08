ff3d305ee38e5e93661b97f5b4b3f4e39c719d56
saniainf <saniainf@gmail.com>
2011-12-08 23:02:33 +0700

    edit glava11

--- Glava11/Chapter1.cs
+++ Glava11/Chapter1.cs
@@ -6,16 +6,24 @@ class Chapter1
     {
         Triangle triangle1 = new Triangle(4.0, 4.0, "ravnobed");
         Triangle triangle2 = new Triangle(8.0, 12.0, "pryamougol");
+        Triangle triangle3 = new Triangle(triangle1);
 
         Console.WriteLine("triangle1");
         triangle1.ShowStyle();
         triangle1.ShowDim();
         Console.WriteLine("Area: {0}", triangle1.Area());
         Console.WriteLine();
+
         Console.WriteLine("triangle2");
         triangle2.ShowStyle();
         triangle2.ShowDim();
         Console.WriteLine("Area: {0}", triangle2.Area());
+        Console.WriteLine();
+
+        Console.WriteLine("triangle3");
+        triangle3.ShowStyle();
+        triangle3.ShowDim();
+        Console.WriteLine("Area: {0}", triangle3.Area());
     }
 }
 
@@ -28,6 +36,12 @@ class object2D
     {
         this.Width = w;
         this.Height = h;
+    }
+
+    public object2D(object2D obj)
+    {
+        this.Width = obj.Width;
+        this.Height = obj.Height;
     }
 
     public void ShowDim()
@@ -43,6 +57,11 @@ class Triangle : object2D
     public Triangle(double width, double height, string style):base (width, height)
     {
         this.Style = style;
+    }
+
+    public Triangle(Triangle obj):base(obj)
+    {
+        this.Style = obj.Style;
     }
 
     public double Area()
