2bd633f2bcc74acf1323791f005af50bff53a7ef
saniainf <saniainf@gmail.com>
2012-03-23 17:30:13 +0700

    wip

--- Glava17/Chapter2.cs
+++ Glava17/Chapter2.cs
@@ -9,21 +9,38 @@ class Chapter2
 {
     public Chapter2()
     {
+        int val;
         Type t = typeof(MyClass); // извлечь объект типа Type из MyClass
         MethodInfo[] mi = t.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly); // занести в массив все методы объекта
+        ConstructorInfo[] ci = t.GetConstructors();
 
-        #region print
+        #region print construct
 
-        Console.WriteLine("Анализ методов в " + t.Name);
+        Console.WriteLine("Доступные конструкторы в " + t.Name);
+        foreach (ConstructorInfo c in ci)
+        {
+            Console.Write(" " + t.Name + "("); // имя
+            ParameterInfo[] pi = c.GetParameters(); // параметры
+            for (int i = 0; i < pi.Length; i++)
+            {
+                Console.Write(pi[i].ParameterType.Name + " " + pi[i].Name);
+                if (i + 1 < pi.Length) Console.Write(", ");
+            }
+            Console.WriteLine(")");
+        }
         Console.WriteLine();
 
-        Console.WriteLine("Поддерживаемые методы: ");
+        #endregion
+
+        #region print methods
+
+        Console.WriteLine("Доступные методы в " + t.Name);
 
         // цикл переборки методов
         foreach (MethodInfo m in mi)
         {
             // возвращаемый тип и имя 
-            Console.Write("   " + m.ReturnType.Name + " " + m.Name + "(");
+            Console.Write("  " + m.ReturnType.Name + " " + m.Name + "(");
 
             // параметры 
             ParameterInfo[] pi = m.GetParameters();
@@ -35,18 +52,39 @@ class Chapter2
             }
 
             Console.WriteLine(")");
-
-            Console.WriteLine();
         }
+        Console.WriteLine();
+
+        #endregion
+
+        #region construct
+
+        int x;
+        for (x = 0; x < ci.Length; x++)
+        {
+            ParameterInfo[] pi = ci[x].GetParameters();
+            if (pi.Length == 2) break;
+        }
+        
+        if (x == ci.Length)
+        {
+            Console.WriteLine("Нет нужного конструктора");
+            return;
+        }
+        else
+            Console.WriteLine("Есть конструктор с 2 параметрами \n");
+
+        // сконструировать объект
+        object[] consargs = new object[2];
+        consargs[0] = 10;
+        consargs[1] = 20;
+        object reflectOb = ci[x].Invoke(consargs);
+
         #endregion
 
         #region invoke methods
 
-        MyClass reflectOb = new MyClass(10, 20);
-        int val;
-
         Console.WriteLine("Вызов методов в " + t.Name);
-        Console.WriteLine();
 
         // переборка всех методов 
         foreach (MethodInfo m in mi)
@@ -73,14 +111,14 @@ class Chapter2
             else if (m.Name.Equals("Sum", StringComparison.Ordinal))
             {
                 val = (int)m.Invoke(reflectOb, null);
-                Console.WriteLine("сумма равна " + val);
+                Console.WriteLine("   " + "сумма равна " + val);
             }
             else if (m.Name.Equals("IsBetween", StringComparison.Ordinal))
             {
                 object[] args = new object[1];
                 args[0] = 14;
                 if ((bool)m.Invoke(reflectOb, args))
-                    Console.WriteLine("14 находится между x and y");
+                    Console.WriteLine("   " + "14 находится между x and y");
             }
             else if (m.Name.Equals("Show", StringComparison.Ordinal))
             {
@@ -97,10 +135,20 @@ class MyClass
     int x;
     int y;
 
+    public MyClass(int i)
+    {
+        Console.WriteLine("Construct class MyClass(int). ");
+        x = y = i;
+        Console.WriteLine();
+    }
+
     public MyClass(int i, int j)
     {
+        Console.WriteLine("Construct class MyClass(int, int). ");
         x = i;
         y = j;
+        Show();
+        Console.WriteLine();
     }
 
     public int Sum()
@@ -116,7 +164,7 @@ class MyClass
 
     public void Set(int a, int b)
     {
-        Console.Write("в методе Set(int, int). ");
+        Console.Write("   " + "в методе Set(int, int). ");
         x = a;
         y = b;
         Show();
@@ -125,7 +173,7 @@ class MyClass
     // перегрузка метода 
     public void Set(double a, double b)
     {
-        Console.Write("в методе Set(double, double). ");
+        Console.Write("   " + "в методе Set(double, double). ");
         x = (int)a;
         y = (int)b;
         Show();
