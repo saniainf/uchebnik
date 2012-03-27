3bdae462d4c8dfcf910262952dab1cc57bf51504
saniainf <saniainf@gmail.com>
2012-03-27 18:00:12 +0700

    wip

--- Glava18/Chapter1.cs
+++ Glava18/Chapter1.cs
@@ -4,6 +4,69 @@ class Chapter1
 {
     public Chapter1()
     {
+        Gen<int> iOb = new Gen<int>(108); //Gen iOb = new Gen(108);
+        iOb.ShowTypeOdj();
+        int intValue = iOb.GetTValue();
+        Console.WriteLine("Значение: " + intValue + "\n");
 
+        Gen<string> strOb = new Gen<string>("text type");
+        strOb.ShowTypeOdj();
+        string strValue = strOb.GetTValue();
+        Console.WriteLine("Значение: " + strValue + "\n");
+
+        // два параметра типа
+
+        TwoGen<string, int> striOb = new TwoGen<string, int>("test text", 847);
+        striOb.ShowTypeOdj();
+        Console.WriteLine("tValue1: {0}", striOb.GetTvalue1());
+        Console.WriteLine("tValue2: {0}", striOb.GetTvalue2());
+    }
+}
+
+class Gen<Tkey>
+{
+    Tkey tValue; // string | int typeObj
+
+    public Gen(Tkey tValue) // public Gen(string | int tValue)
+    {
+        this.tValue = tValue;
+    }
+
+    public Tkey GetTValue()
+    {
+        return tValue;
+    }
+
+    public void ShowTypeOdj()
+    {
+        Console.WriteLine("Тип в tValue " + typeof(Tkey));
+    }
+}
+
+class TwoGen<Tkey1, Tkey2>
+{
+    Tkey1 tValue1;
+    Tkey2 tValue2;
+
+    public TwoGen(Tkey1 tValue1, Tkey2 tValue2)
+    {
+        this.tValue1 = tValue1;
+        this.tValue2 = tValue2;
+    }
+
+    public Tkey1 GetTvalue1()
+    {
+        return tValue1;
+    }
+
+    public Tkey2 GetTvalue2()
+    {
+        return tValue2;
+    }
+
+    public void ShowTypeOdj()
+    {
+        Console.WriteLine("Тип в tValue1 " + typeof(Tkey1));
+        Console.WriteLine("Тип в tValue2 " + typeof(Tkey2));
     }
 }
--- Tools/_ListS.cs
+++ Tools/_ListS.cs
@@ -1,63 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class _ListS<T>
+namespace _List
 {
-    T[] BaseArray;
-    int Indx;
-    int Size;
-
-    public _ListS(int size)
+    class _ListS<T>
     {
-        BaseArray = new T[size];
-        this.Size = size;
-        Indx = 0;
-    }
+        T[] BaseArray;
+        int Indx;
+        int Size;
 
-    public T this[int index]
-    {
-        // This is the get accessor. 
-        get
+        public _ListS(int size)
         {
-            return BaseArray[index];
+            BaseArray = new T[size];
+            this.Size = size;
+            Indx = 0;
         }
 
-        // This is the set accessor. 
-        set
+        public T this[int index]
         {
-            BaseArray[index] = value;
+            // This is the get accessor. 
+            get
+            {
+                return BaseArray[index];
+            }
+
+            // This is the set accessor. 
+            set
+            {
+                BaseArray[index] = value;
+            }
         }
-    }
 
-    public void InsertAt(int i, T item)
-    {
-        T[] tmpArr = new T[Size];
-        //скопировать в tmpArr от 0 до indx, затем item, затем от i + 1 до Indx
-    }
+        public void InsertAt(int i, T item)
+        {
+            T[] tmpArr = new T[Size];
+            //скопировать в tmpArr от 0 до indx, затем item, затем от i + 1 до Indx
+        }
 
-    public void Insert(int indx)
-    {
+        public void Insert(int indx)
+        {
 
-    }
+        }
 
-    public void RemoveAt(int indx, T item)
-    {
+        public void RemoveAt(int indx, T item)
+        {
 
-    }
+        }
 
-    public void Remove(int indx)
-    {
+        public void Remove(int indx)
+        {
 
-    }
+        }
 
-    public void Clear()
-    {
+        public void Clear()
+        {
 
-    }
+        }
 
-    public void Fill(T item)
-    {
+        public void Fill(T item)
+        {
 
+        }
     }
 }
-
